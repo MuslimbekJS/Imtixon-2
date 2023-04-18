512f9b39a89b0ed80f7d33af3b86f121b78cd6fd
“MuslimbekJS” <reactjs077@gmail.com>
2023-04-17 20:06:20 -0700

    first commit

---
+++ js/owl-carouse.js
@@ -0,0 +1,28 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            nav: true
+        },
+        100: {
+            items: 1,
+            nav: true
+        },
+        400: {
+            items: 2,
+            nav: true
+        },
+        600: {
+            items: 2,
+            nav: true
+        },
+        1000: {
+            items: 4,
+            nav: true,
+            loop: true
+        }
+    }
+})
